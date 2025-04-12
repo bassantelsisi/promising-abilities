fba85f964717712b7982f1f3e2d9011baab3944b
Bassant <bassantelsisi2022@gmail.com>
2025-04-12 02:31:47 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="app-container min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow">
